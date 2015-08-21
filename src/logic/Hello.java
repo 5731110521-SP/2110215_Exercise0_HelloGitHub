6ba7d6df16dcba2e80d0d065b036aca3d4490218
Sopita Punyakham <amint.dinger@gmail.com>
2015-08-21 10:17:53 +0700

    Implement method.

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -15,11 +15,23 @@ public class Hello {
 
 	public static String greetingMessage(){
 		// TODO Implement this method to return string "Hello GitHub".
-		return "";
+		return "Hello GitHub";
 	}
 	
 	public static int[] bubbleSort(int[] array){
-		// TODO Implement this method to sort the given array in an ascending order and return the sorted array.
+		Boolean isSwap;
+		do{
+			isSwap = false;
+			for(int i =0;i<array.length -1 ; i++){
+				if(array[i]>array[i+1]){
+					int temp = array[i];
+					array[i] = array[i+1];
+					array[i+1] = temp;
+					isSwap = true;
+				}
+			}
+		}
+		while(isSwap);
 		return array;
 	}
 	
